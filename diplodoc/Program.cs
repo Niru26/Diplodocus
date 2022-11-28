62fdf00f2aad232e14723bfa552bb50bca5dd2b7
Nikolay <nikolayrudakov@mail.ru>
2022-11-28 22:11:57 +0600

    start working on dot programm, there is a problem, asked t.

--- diplodoc/Program.cs
+++ diplodoc/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("12 Hello, World!");
+﻿Console.Clear();
+
+int xa = 40, ya =1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+int x = xa, y = yb;
+int count = 0;
+
+while (count < 10000) {
+    int randomiser = new Random().Next(0, 3);
+    if (randomiser == 0) 
+    {
+        x = (x + xa) /2;
+        y = (y + ya) /2;
+    }
+    if (randomiser == 1) 
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2; 
+    }
+     if (randomiser == 2) 
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("X");
+    count++;
+};
+
--- stegosaurus/Program.cs
+++ stegosaurus/Program.cs
@@ -1,1 +1,2 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.SetCursorPosition(200, 10);
+Console.WriteLine('@');
