983890c7e87306bcee648ad1c46e4fbe372b753e
Nikolay <nikolayrudakov@mail.ru>
2022-11-30 01:08:01 +0600

    check stegosaurus folder for seminar 1 tasks

--- stegosaurus/Program.cs
+++ stegosaurus/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+// Задача 1
+// Console.Write($"Enter your number one ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Enter your number two ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad = number2 * number2;
+
+// if (number1 == quad) {
+//     Console.WriteLine("Yes! You are right!");
+// } else {
+//     Console.WriteLine("Nope, think, Vasya, think...");
+// }
+
+// Задача 2
+// Console.Write("Enter range number ");
+// int range = Convert.ToInt32(Console.ReadLine());
+
+// int counter = -range;
+
+// while(counter <= range) {
+//     Console.Write($"{counter} ");
+//     counter++;
+// }
+
+
+
+
+
+
