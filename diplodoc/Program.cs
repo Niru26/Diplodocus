e1142ba5d05fcdcb25b368014c0f2a58e447ffb2
Nikolay <nikolayrudakov@mail.ru>
2022-12-01 16:42:34 +0600

    add new folder - alamosaurus, for chapter 4 exersises

--- diplodoc/Program.cs
+++ diplodoc/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+// int i = 300;
+// byte b = (byte) i;
+
+// var sigh = "1234";
+// int numberSign = Convert.ToUInt16(sigh);
+
+// try
+// {
+//     var sighTwo = "1234";
+//     byte myByte= Convert.ToByte(sighTwo);
+//     Console.WriteLine(myByte);
+// }
+// catch (System.Exception)
+// {
+//     Console.WriteLine("This can't be casting or converted");
+// }
+// try
+// {
+//     var sighThree= "false";
+//     bool myBool= Convert.ToBoolean(sighThree);
+//     Console.WriteLine(myBool);
+// }
+// catch (System.Exception)
+// {
+//     Console.WriteLine("This can't be casting or converted");
+// }
+
+// Console.WriteLine(b);
+// Console.WriteLine(numberSign);
+
+int aPost = 1;
+int bPost = aPost++;
+
+Console.WriteLine("{0} {1}", aPost, bPost );
+Console.WriteLine(bPost);
+
+int aPre = 9;
+int bPre = ++aPre;
+
+Console.WriteLine("{0} {1}", aPre, bPre );
+Console.WriteLine(bPre);
+
+int a = 1;
+int b = 2;
+Console.WriteLine("Result is " + (float)a / (float)b);
+
+
+
+
+
+
