f4af5fb5966d88215c3ae11a8336e9c219185417
Nikolay <nikolayrudakov@mail.ru>
2022-12-03 14:40:30 +0600

    start working on homework 1

---
+++ zuniceratops/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+//Task 2
+int requestData() {
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// int inputData = requestData();
+// Console.WriteLine(inputData);
+// int inputData = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(inputData);
+int showMax() {
+    int inputData1 = requestData();
+    int inputData2 = requestData();
+    if (inputData1 > inputData2) {
+        return inputData1;
+    }
+    return inputData2;
+}
+
+int maxValue = showMax();
+Console.WriteLine(maxValue);
+
+
+
