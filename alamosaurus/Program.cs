13ef316944aca5a169c41ed8bdebfefd5c8d2140
Nikolay <nikolayrudakov@mail.ru>
2022-12-03 12:00:49 +0600

    start new day, must finish homework 1 for C#

--- alamosaurus/Program.cs
+++ alamosaurus/Program.cs
@@ -1,7 +1,24 @@
-﻿Console.Clear();
+﻿// int Add(int a, int b) {
+//     return a + b;
+// } 
+// int result = Add (2, 3);
+// Console.WriteLine(result);
 
-int Add(int a, int b) {
-    return a + b;
-} 
-int result = Add (2, 3);
-Console.WriteLine(result);
+
+internal class Program
+{
+      private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Person john = new Person();
+        john.FirstName = "Tomotoli";
+        john.LastName = "Kababic";
+        john.Introduce();
+
+        Calculator calculator = new Calculator();
+        double result = calculator.Add(2, 3);
+        Console.WriteLine(result);
+    }
+
+}
--- albertaceratops/Program.cs
+++ albertaceratops/Program.cs
@@ -1,22 +1,25 @@
 ﻿Console.Clear();
 
-byte smallNumber = 3;
-int integerNumber = 234;
-float myHight = 181.5F;
-double number = 255.21;
-char symbol = 'A';
-string myName = "Nikolai";
-bool switchedOn = false;
-bool switchedOff = true;
-const float Pi = 3.14f;
+// byte smallNumber = 3;
+// int integerNumber = 234;
+// float myHight = 181.5F;
+// double number = 255.21;
+// char symbol = 'A';
+// string myName = "Nikolai";
+// bool switchedOn = false;
+// bool switchedOff = true;
+// const float Pi = 3.14f;
 
-Console.WriteLine($"It is number {smallNumber}");
-Console.WriteLine(integerNumber);
-Console.WriteLine($"Not excatly {myHight}");
-Console.WriteLine($"Double double {number}");
-Console.WriteLine(symbol);
-Console.WriteLine(myName);
-Console.WriteLine(switchedOn);
-Console.WriteLine($"{switchedOff} {myName} {symbol}");
-Console.WriteLine("{0} {1} {2}", int.MaxValue, byte.MaxValue, double.MaxValue);
-Console.WriteLine(Pi);
+// Console.WriteLine($"It is number {smallNumber}");
+// Console.WriteLine(integerNumber);
+// Console.WriteLine($"Not excatly {myHight}");
+// Console.WriteLine($"Double double {number}");
+// Console.WriteLine(symbol);
+// Console.WriteLine(myName);
+// Console.WriteLine(switchedOn);
+// Console.WriteLine($"{switchedOff} {myName} {symbol}");
+// Console.WriteLine("{0} {1} {2}", int.MaxValue, byte.MaxValue, double.MaxValue);
+// Console.WriteLine(Pi);
+
+int[] numbers = new int[5] {1, 2, 3, 4, 5};
+Console.WriteLine(numbers);
