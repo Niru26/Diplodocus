7b3ee26e1c884bb56dbb6e706697bdf95eeda727
Nikolay <nikolayrudakov@mail.ru>
2022-12-08 01:21:46 +0600

    in albertaceraptos folder start to work on C# homework 3

--- albertaceratops/Program.cs
+++ albertaceratops/Program.cs
@@ -1,25 +1,6 @@
 ﻿Console.Clear();
 
-// byte smallNumber = 3;
-// int integerNumber = 234;
-// float myHight = 181.5F;
-// double number = 255.21;
-// char symbol = 'A';
-// string myName = "Nikolai";
-// bool switchedOn = false;
-// bool switchedOff = true;
-// const float Pi = 3.14f;
+string myNumber = Console.ReadLine();
+int stringLength = Convert.ToInt32(myNumber.Length);
 
-// Console.WriteLine($"It is number {smallNumber}");
-// Console.WriteLine(integerNumber);
-// Console.WriteLine($"Not excatly {myHight}");
-// Console.WriteLine($"Double double {number}");
-// Console.WriteLine(symbol);
-// Console.WriteLine(myName);
-// Console.WriteLine(switchedOn);
-// Console.WriteLine($"{switchedOff} {myName} {symbol}");
-// Console.WriteLine("{0} {1} {2}", int.MaxValue, byte.MaxValue, double.MaxValue);
-// Console.WriteLine(Pi);
-
-int[] numbers = new int[5] {1, 2, 3, 4, 5};
-Console.WriteLine(numbers);
+Console.WriteLine($"{stringLength} {myNumber[4]}");
