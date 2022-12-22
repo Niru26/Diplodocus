eb6b5daa53cd4965a4bcad574f55a8cc2158ba17
Nikolay <nikolayrudakov@mail.ru>
2022-12-22 11:56:50 +0600

    feat: finished work on Homework 6

--- paralititan/Program.cs
+++ paralititan/Program.cs
@@ -24,4 +24,30 @@ countPositiveNumbers();
 Console.WriteLine("End Task 41");
 Console.WriteLine(' ');
 // Task 43
+double requestDoubleData() {
+    return Convert.ToDouble(Console.ReadLine());
+}
+
 Console.WriteLine("Task 43");
+void checkCrossing() {
+    Console.WriteLine("Enter equation parameters:");
+    Console.WriteLine("Enter 1st line b1:");
+    double b1 = requestDoubleData();
+    Console.WriteLine("Enter 1st line k1:");
+    double k1 = requestDoubleData();
+    Console.WriteLine("Enter 2nd line b2:");
+    double b2 = requestDoubleData();
+    Console.WriteLine("Enter 2nd line k2:");
+    double k2 = requestDoubleData();
+    
+    if(k1 == k2) {
+        Console.WriteLine("Lines dosn't crossing");
+        return;
+    }
+    double y = (-k2 / k1 * b1 + b2) / (1 - k2 / k1);
+    double x = (y - b1) / k1;
+    double roundedX = Math.Round(x, 1);
+    double roundedY = Math.Round(y, 1);
+    Console.WriteLine($"Crossing coordinates: {roundedX}; {roundedY}");
+}
+checkCrossing();
