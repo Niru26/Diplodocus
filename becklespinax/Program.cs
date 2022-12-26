2075e798ee48d2fdc4ef45f8b9686d2e75c67371
Nikolay <nikolayrudakov@mail.ru>
2022-12-26 20:45:38 +0600

    feat: work on homework 7 task 47 save 1

--- becklespinax/Program.cs
+++ becklespinax/Program.cs
@@ -1,2 +1,13 @@
 ﻿Console.Clear();
 Console.WriteLine("Homework 7 by Rudakov N.");
+//Task 47
+Console.WriteLine("Task 47");
+
+int getInt32Data() {
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double genrateDoubleNumber(int minimum, int maximum) {
+    int numberBase = new Random().Next(maximum, maximum);
+    double number = 0;    
+}
