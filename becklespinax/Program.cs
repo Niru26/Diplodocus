5f016ba983d66edb6af81f6b13825f4da4962ff2
Nikolay <nikolayrudakov@mail.ru>
2022-12-26 20:56:51 +0600

    feat: work on homework 7 task 47 save 2

--- becklespinax/Program.cs
+++ becklespinax/Program.cs
@@ -7,7 +7,15 @@ int getInt32Data() {
     return Convert.ToInt32(Console.ReadLine());
 }
 
-double genrateDoubleNumber(int minimum, int maximum) {
-    int numberBase = new Random().Next(maximum, maximum);
-    double number = 0;    
+double genrateDoubleNumber() {
+    int minimum = getInt32Data();
+    int maximum = getInt32Data();
+    int numberBase = new Random().Next(minimum, maximum);
+    double numberDecimalPart = new Random().NextDouble();
+    double number = numberBase + Math.Round(numberDecimalPart, 2);
+    return number;    
 }
+
+
+
+Console.WriteLine(genrateDoubleNumber());
