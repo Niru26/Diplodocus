1e651964e36a453ca8a291a5cb0477f2040355e8
Nikolay <nikolayrudakov@mail.ru>
2022-12-28 22:29:54 +0600

    feat: start work on homework 8, 1st task solved

--- chasmosaurus/Program.cs
+++ chasmosaurus/Program.cs
@@ -1,12 +1,14 @@
 ﻿Console.Clear();
 Console.WriteLine("Homework 8 by Rudakov N.");
-Console.WriteLine("Task 54");
-// Task 54
-int getData() {
+// Console.WriteLine("Task 54");
+// // Task 54
+int getData()
+{
     return Convert.ToInt32(Console.ReadLine());
 }
 
-int [,] generate2dIntegerArray() {
+int[,] generate2dIntegerArray()
+{
     Console.WriteLine("Enter array min & max:");
     int minimum = getData();
     int maximum = getData();
@@ -15,22 +17,60 @@ int [,] generate2dIntegerArray() {
     int columns = getData();
     Console.WriteLine(' ');
     int[,] array = new int[rows, columns];
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < columns; j++) {
-            array[i,j] = new Random().Next(minimum, maximum);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(minimum, maximum);
         }
     }
     return array;
 }
 
-void show2dIntegerArray(int[,] array) {
-    for(int i = 0; i < array.GetLength(0); i++) {
-        for(int j = 0; j < array.GetLength(1); j++ ) {
-            Console.Write(array[i,j] + "\t");
+void show2dIntegerArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
         }
         Console.WriteLine(' ');
     }
     Console.WriteLine(' ');
 }
 
-show2dIntegerArray(generate2dIntegerArray());
+// int[,] sortArrayRows(int[,] array)
+// {
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             for (int k = j + 1; k < array.GetLength(1); k++)
+//             {
+//                 if (array[i, j] < array[i, k])
+//                 {
+//                     int temporary = array[i, j];
+//                     array[i, j] = array[i, k];
+//                     array[i, k] = temporary;
+//                 }
+//             }
+//         }
+//     }
+//     return array;
+// }
+
+// int[,] initialArray = generate2dIntegerArray();
+// Console.WriteLine("Initial array:");
+// show2dIntegerArray(initialArray);
+// Console.WriteLine("Sorted array:");
+// int[,] sortedArray = sortArrayRows(initialArray);
+// show2dIntegerArray(sortedArray);
+// Console.WriteLine("End task 54");
+// Task 56
+Console.WriteLine("Task 56");
+
+int sumRow
+
